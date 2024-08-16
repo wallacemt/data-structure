afc094d7245f09463d40d5d451eb4d901453d361
Wallace Santana <wallacesantanak0@gmail.com>
2024-08-16 16:42:09 -0300

    comentando os codigos

--- 1- variaveis-e-memoria/main.c
+++ 1- variaveis-e-memoria/main.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-    int a = 10;
-    int b, c;
+    int a = 10;// inicializa a variavel
+    int b, c = 0; // inicializa as variaveis
 
-    printf("&a = %p, a = %d\n", &a, a);
+    printf("&a = %p, a = %d\n", &a, a);// mostra o endereço da variavel e o valor dela
     printf("&b = %p, b = %d\n", &b, b);
     printf("&c = %p, c = %d\n\n", &c, c);
 
-    b = 20;
-    c = a + b;
+    b = 20;// atribui um novo valor
+    c = a + b;// atribui um novo valor
 
-    printf("&a = %p, a = %d\n", &a, a);
+    printf("&a = %p, a = %d\n", &a, a);// mostra o endereço da variavel e o valor dela apos alteração
     printf("&b = %p, b = %d\n", &b, b);
     printf("&c = %p, c = %d\n\n", &c, c);
     return 0;
