f337f120af5b2500a6e645e731272e50d4d5e406
Wallace Santana <138271229+wallacemt@users.noreply.github.com>
2024-08-20 18:26:11 -0300

    Update main.c

--- 2- ponteiros/main.c
+++ 2- ponteiros/main.c
@@ -10,14 +10,14 @@ int main()
 	scanf("%d", &tamanho);//armazena o dado que o usuario digitou
 
 	vetor = (int*)malloc(tamanho*sizeof(int)); //cria o espaço na memoria referente ao valor do tamanho
-    int *head = vetor;//endereço do inicio do vetor
+    	int *head = vetor;//endereço do inicio do vetor
     
 	for(int i = 0; i < tamanho; i++) {
 		(*vetor) = i * 2; //armazeda dados no vetor
 		vetor++;// avança para o proximo endereço de memoria
 	}
 
-    vetor = head;//retorna o vetor para posicao de memoria inicial
+    	vetor = head;//retorna o vetor para posicao de memoria inicial
     
 	for(int i = 0; i < tamanho; i++) {
 		printf("vetor[%d]: %d\n", i, vetor[i]);//mostra os dados do vetor para o usuario
