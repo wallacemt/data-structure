89d402d73c07d8ecd4c2a81224a49220dd45f20f
Wallace Santana <wallacesantanak0@gmail.com>
2024-09-05 16:06:13 -0300

    estrutura de fila concluida

--- 4- filas(FIFO)/main.c
+++ 4- filas(FIFO)/main.c
@@ -5,10 +5,11 @@
 
 typedef struct node{
     int value;
-    struct node *next
+    struct node *next;
 } Node;
 
 void insertNode(Node **fila, int num){
+    system("clear");
     Node *aux, *new = malloc(sizeof(Node));
     if(new){
         new->value = num;
@@ -28,28 +29,72 @@ void insertNode(Node **fila, int num){
 }
 
 Node* removeNode(Node **fila){
-    Node *remove = NULL;
+    system("clear");
+    Node *remover = NULL;
 
     if(*fila){
-        remove = *fila;
-        *fila = remove->next;
+        remover = *fila;
+        *fila = remover->next;
     }else{
         printf("\tFila vazia\n ");
     }
-    return remove;
+    return remover;
 
 
 }
 
 void printFila(Node *fila){
-    printf("\t-------- FILA ----------\n");
+    system("clear");
+    printf("-------- FILA ----------\n");
+    int i = 1;
+    while(fila) {
+        printf("%d - %d \n",i, fila->value);
+        fila = fila ->next;
+        i++;
+    }
     
-    printf("\t-------- FIM FILA ----------\n");
+    printf("-------- FIM FILA ----------\n");
     
 }
 
 int main()
 {
-
+    Node *r, *fila = NULL;
+    int opcao, valor;
+    
+    do{
+        
+        printf("\n\t0 - Sair\n\t1 - Inserir Dado\n\t2 - Remover Dado\n\t3 - Imprimir FILA\n\tEscolha uma opção: ");
+        scanf("%d", &opcao);
+        switch(opcao){
+            case 1:
+                printf("Digite um valor!:");
+                scanf("%d", &valor);
+                insertNode(&fila, valor);
+                break;
+            case 2:
+                r = removeNode(&fila);
+                if(r != NULL){
+                    printf("Removido: %d\n", r->value);
+                    free(r);
+                }
+                break;
+            case 3:
+                printFila(fila);
+                break;
+            default:
+                if(opcao!= 0){
+                    printf("Opção Invalida!");
+                }
+        }
+    }while(opcao != 0);
+    
     return 0;
 }
+
+
+
+
+
+
+
